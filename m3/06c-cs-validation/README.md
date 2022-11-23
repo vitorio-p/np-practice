8c9c07c411df8c01f96d5cc23b13055a2510fca5
vitorio-p <granz.35@gmail.com>
2022-11-23 12:23:28 +0800

    working skeleton for ex 06c

---
+++ m3/06c-cs-validation/README.md
@@ -0,0 +1,21 @@
+Instructions
+Following from Lab Sheet - 6C
+
+When we perform form validation, we have to check on both the client side (using the browser-based JavaScript) and on the server side (in the Express).
+
+Create your own HTML based on the following business logic for reporting lost and found
+
+
+Logic
+
+1. The user must enter the name of the item, and it must be more than 3 characters but less than 200 characters
+
+2. Provides an email address that has at least a '@" character, a '.' character
+
+3. Using radio buttons, the user can select whether they left the item in a taxi, bus or MRT. The user must select at least one
+
+4. Using checkboxes, the user can select the various properties of the lost item, from the choices of "bulky", "expensive", "colorful", "branded", "small" and "plain". Only at most 3 checkboxes can be selected and at least one must be selected.
+
+Challenge
+
+For part 3, have a radio button that says "others". If the user clicks on that, they must then fill in another textbox to provide more details.
--- m3/06c-cs-validation/index.js
+++ m3/06c-cs-validation/index.js
@@ -12,11 +12,62 @@ app.use(express.urlencoded({
     'extended':false
 }))
 
+let itemRecords = [
+    {
+        id: 1,
+        itemName: 'iPhone',
+        email: 'vito@fp.com',
+        location: 'Taxi',
+        properties: ['Expensive', 'Branded', 'Small']
+    },
+    {
+        id: 2,
+        itemName: 'Brown bag',
+        email: 'ken@fp.com',
+        location: 'MRT',
+        properties: ['Plain']
+    },
+]
+
 // routes
-app.get('/', function(req,res){
-    res.render("home")
+app.get('/', function(req, res){
+    let allItems = itemRecords;
+    res.render("home", {
+        allItems: allItems
+    });
+});
+
+app.get('/add-item', function(req, res){
+    res.render("add-item");
+});
+
+app.post('/add-item', function(req, res) {
+    let itemName = req.body.itemName;
+    let email = req.body.email;
+    let location = req.body.location;
+
+    let properties = [];
+    if (req.body.properties) {
+        if (Array.isArray(req.body.properties)) {
+            properties = req.body.properties;
+        } else {
+            properties.push(req.body.properties);
+        }
+    };
+
+    let newItem = {
+        id: Math.floor(Math.random() * 10000 + 1),
+        itemName: itemName,
+        email: email,
+        location: location,
+        properties: properties
+    }
+
+    itemRecords.push(newItem);
+
+    res.redirect('/')
 })
 
 app.listen(3000, function(){
     console.log("server started");
-})
+});
