17d67c7dc6c3ff4fcf6b9151cc14eafc6b7bd4b5
vitorio-p <vitorio.pan@gmail.com>
2022-11-24 14:58:19 +0800

    + client-side and server-side validation

--- m3/06c-cs-validation/index.js
+++ m3/06c-cs-validation/index.js
@@ -1,73 +1,118 @@
-const express = require('express');
-const hbs = require('hbs');
-const waxOn = require('wax-on');
-
+const express = require("express");
+const hbs = require("hbs");
+const waxOn = require("wax-on");
+const { check, validationResult } = require("express-validator");
 
 let app = express();
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 waxOn.on(hbs.handlebars);
-waxOn.setLayoutPath('./views/layouts');
+waxOn.setLayoutPath("./views/layouts");
 
-app.use(express.urlencoded({
-    'extended':false
-}))
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
 let itemRecords = [
-    {
-        id: 1,
-        itemName: 'iPhone',
-        email: 'vito@fp.com',
-        location: 'Taxi',
-        properties: ['Expensive', 'Branded', 'Small']
-    },
-    {
-        id: 2,
-        itemName: 'Brown bag',
-        email: 'ken@fp.com',
-        location: 'MRT',
-        properties: ['Plain']
-    },
-]
+  {
+    id: 1,
+    itemName: "iPhone",
+    email: "vito@fp.com",
+    location: "Taxi",
+    properties: ["Expensive", "Branded", "Small"],
+  },
+  {
+    id: 2,
+    itemName: "Brown bag",
+    email: "ken@fp.com",
+    location: "MRT",
+    properties: ["Plain"],
+  },
+];
+
+// client-side validation
+
+/*
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+
+  */
 
 // routes
-app.get('/', function(req, res){
-    let allItems = itemRecords;
-    res.render("home", {
-        allItems: allItems
-    });
+app.get("/", function (req, res) {
+  let allItems = itemRecords;
+  res.render("home", {
+    allItems: allItems,
+  });
 });
 
-app.get('/add-item', function(req, res){
-    res.render("add-item");
+app.get("/add-item", function (req, res) {
+  res.render("add-item");
 });
 
-app.post('/add-item', function(req, res) {
-    let itemName = req.body.itemName;
-    let email = req.body.email;
-    let location = req.body.location;
+// issues with express-validator
+app.post(
+  "/add-item",
+  check("itemName").isLength({ min: 3, max: 200 }),
+  check("email").isEmail(),
+  check("location").not().isEmpty(),
+  check("properties").not().isEmpty(),
+  function (req, res) {
+    const err = validationResult(req);
 
-    let properties = [];
-    if (req.body.properties) {
+    if (!err.isEmpty()) {
+      console.log(err.mapped());
+    } else {
+      let itemName = req.body.itemName;
+      let email = req.body.email;
+      let location = req.body.location;
+
+      let properties = [];
+      if (req.body.properties) {
         if (Array.isArray(req.body.properties)) {
-            properties = req.body.properties;
+          properties = req.body.properties;
         } else {
-            properties.push(req.body.properties);
+          properties.push(req.body.properties);
         }
-    };
+      }
 
-    let newItem = {
+      if (properties.length > 3) {
+        throw "Too many properties. Please return to the previous page.";
+      }
+
+      let newItem = {
         id: Math.floor(Math.random() * 10000 + 1),
         itemName: itemName,
         email: email,
         location: location,
-        properties: properties
+        properties: properties,
+      };
+
+      itemRecords.push(newItem);
+
+      res.redirect("/");
     }
+  }
+);
 
-    itemRecords.push(newItem);
-
-    res.redirect('/')
-})
-
-app.listen(3000, function(){
-    console.log("server started");
+app.listen(3000, function () {
+  console.log("server started");
 });
