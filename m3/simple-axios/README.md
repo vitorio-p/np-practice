41933b412b0283b6c4f2563491ce29e3be75a5ac
vitorio-p <vitorio.pan@gmail.com>
2022-11-28 16:17:15 +0800

    setup working CRUD, taking data from local db

---
+++ m3/simple-axios/README.md
@@ -0,0 +1,12 @@
+Instructions
+Create suitable endpoints that allow for a movie CRUD application.
+
+Using AXIOS or fetch and together with the RESTDB.io RESTful interface, create suitable routes to fit the following
+
+You must have the following routes
+
+A route to display all the movies
+A route to allow the user to create a new movie
+A route to allow the user to edit an existing movie
+A route to allow the user to delete a movie
+Note: Perform adequate and suitable data validation on front-end and back-end
---
+++ m3/simple-axios/index.js
@@ -0,0 +1,131 @@
+const express = require("express");
+const hbs = require("hbs");
+const waxOn = require("wax-on");
+
+let app = express();
+app.set("view engine", "hbs");
+waxOn.on(hbs.handlebars);
+waxOn.setLayoutPath("./views/layouts");
+
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+// local db
+// TODO implement axios and db
+let movieRecords = [
+  {
+    id: 1,
+    movieTitle: "The Shawshank Redemption",
+    genre: "Drama",
+    duration: 162,
+    rating: 9.2,
+  },
+  {
+    id: 2,
+    movieTitle: "The Godfather",
+    genre: "Drama",
+    duration: 175,
+    rating: 9.2,
+  },
+];
+
+// routes
+app.get("/", function (req, res) {
+  let allMovies = movieRecords;
+  res.render("home", {
+    allMovies: allMovies,
+  });
+});
+
+app.get("/add", function (req, res) {
+  res.render("add");
+});
+
+app.post("/add", function (req, res) {
+  let movieTitle = req.body.movieTitle;
+  let genre = req.body.genre;
+  let duration = req.body.duration;
+  let rating = req.body.rating;
+
+  let newMovie = {
+    id: Math.floor(Math.random() * 10000 + 1),
+    movieTitle: movieTitle,
+    genre: genre,
+    duration: duration,
+    rating: rating,
+  };
+
+  movieRecords.push(newMovie);
+
+  res.redirect("/");
+});
+
+app.get("/edit/:movie_record_id", function (req, res) {
+  let id = req.params.movie_record_id;
+  let tempRecord = null;
+  for (let record of movieRecords) {
+    movieRecord = record;
+    if (record.id == id) {
+      tempRecord = record;
+      break;
+    }
+  }
+  res.render("edit", {
+    movieRecord: tempRecord,
+  });
+});
+
+app.post("/edit/:movie_record_id", function (req, res) {
+  let tempRecord = {
+    movieTitle: req.body.movieTitle,
+    genre: req.body.genre,
+    duration: req.body.duration,
+    rating: req.body.rating,
+  };
+
+  let index = -1;
+
+  for (let i = 0; i < movieRecords.length; i++) {
+    if (movieRecords[i].id == req.params.movie_record_id) {
+      index = i;
+      break;
+    }
+  }
+
+  movieRecords[index] = tempRecord;
+
+  res.redirect("/");
+});
+
+app.get("/delete/:movie_record_id", function (req, res) {
+  let movieRecord = movieRecords.find(function (record) {
+    if (record.id == req.params.movie_record_id) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  res.render("delete", {
+    movieRecord: movieRecord,
+  });
+});
+
+app.post("/delete/:movie_record_id", function (req, res) {
+  let indexToDelete = movieRecords.findIndex(function (record) {
+    if (record.id == req.params.movie_record_id) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  movieRecords.splice(indexToDelete, 1);
+  res.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log("server started");
+});
